65998b2b4ff8b282f674e6a0b863b5831848f61e
deepika02012001 <deepikak01022001@gmail.com>
2021-06-22 21:19:53 +0530

    added delete task

--- Task-management/index.js
+++ Task-management/index.js
@@ -1,20 +1,20 @@
 const taskContainer=document.querySelector(".task__container")
 //since 2 divs are there with same parent class it displays output as array when we use getElementsByClassName so we use query selector
-const globalStore=[];
+var globalStore=[];
 
 const generateNewCard=(taskData)=>`
-<div class="col-md-6 col-lg-4" id=${taskData.id}><!--medium screen need cards of width 6 and large one is width 4-->
+<div class="col-md-6 col-lg-4"><!--medium screen need cards of width 6 and large one is width 4-->
 <div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
         <button type="button" class="btn btn-outline-danger"><i class="fas fa-pencil-alt"></i></button>
-        <button type="button" class="btn btn-outline-warning"><i class="fas fa-trash-alt"></i></button>
+        <button type="button" class="btn btn-outline-warning" onclick="deleteCard.apply(this,arguments)" id=${taskData.id}><i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i></button>
     </div>
     <img src=${taskData.imageUrl} class="card-img-top" alt="books">
     <div class="card-body">
       <h5 class="card-title">${taskData.taskTitle}</h5>
       <p class="card-text">${taskData.taskType}</p>
       <p class="card-description">${taskData.taskDescription}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
     </div>
     <div class="card-footer d-flex justify-content-end">
         <button type="button" class="btn btn-primary">Open Task</button>
@@ -46,13 +46,30 @@ const saveChanges=()=>{
     };
     //console.log(taskData);
 taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
-
 //access local storage to store data in globalStore(above) and not delete anything on refresh
 globalStore.push(taskData);
 localStorage.setItem("tasky", JSON.stringify({cards:globalStore})) //id,data
 //localStorage.setItem("tasky", taskData), it is wrong because everytime local storage stores a new card by replacement
 //cards is array of objects
+};
 
+const deleteCard=(event)=>{
+    event=window.event;
+    //get element id
+    const targetID=event.target.id;
+    const tagname=event.target.tagName;
+    //match id with id inside global store
+    globalStore=globalStore.filter((cardObject)=>cardObject.id!=targetID);
+    //if match found remove it
+    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}))
+    //contact parent
+    //taskContainer.removeChild()
+    if(tagname==="BUTTON"){
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    }
+    else{
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    }
 };
 
 
