b4d49bbd5f18282aa3125fc9278293d136618351
deepika02012001 <deepikak01022001@gmail.com>
2021-07-29 23:00:11 +0530

    added tmdb api

--- book-my-show/src/components/HeroCarousel/HeroCarousel.component.jsx
+++ book-my-show/src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import HeroSlider from "react-slick";
+import axios from "axios";
 
 //component
 import { NextArrow,PrevArrow } from "./Arrows.component";
 
 const HeroCarousel = () => {
+
+    const [images,setImages]=useState([]);
+    
+    useEffect(()=>{
+      //async call
+      const requestNowPlayingMovies=async ()=>{
+        const getImages=await axios.get("/movie/now_playing");
+        setImages(getImages.data.results);
+      };
+      requestNowPlayingMovies();
+
+    },[]);
+
     const settingsLG = {
       arrows: true,
       autoplay: true,
@@ -29,6 +43,7 @@ const HeroCarousel = () => {
       prevArrow: <PrevArrow />,
     };
 
+    /*
     const images=[
         "https://images.pexels.com/photos/3137890/pexels-photo-3137890.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         "https://images.pexels.com/photos/269140/pexels-photo-269140.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
@@ -36,13 +51,15 @@ const HeroCarousel = () => {
         "https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         "https://images.pexels.com/photos/1200450/pexels-photo-1200450.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     ];
+    */
+
     return (
         <>
           <div className="lg:hidden">
             <HeroSlider {...settings}>
               {images.map((image) => (
                 <div className="w-full h-56 md:h-80 py-3">
-                  <img src={image} alt="testing" className="w-full h-full" />
+                  <img src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`} alt="testing" className="w-full h-full" />
                 </div>
               ))}
             </HeroSlider>
@@ -53,7 +70,7 @@ const HeroCarousel = () => {
               {images.map((image) => (
                 <div className="w-full h-96 px-2 py-3">
                   <img
-                    src={image}
+                    src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
                     alt="testing"
                     className="w-full h-full rounded-md"
                   />
--- book-my-show/src/pages/Home.page.jsx
+++ book-my-show/src/pages/Home.page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Slider from "react-slick";
 // Components
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
@@ -7,8 +7,38 @@ import SimpleInfo from "../components/SimpleInfo/SimpleInfo.component";
 //config
 import TempPosters from "../config/TempPosters.config";
 import PosterSlider from "../components/PosterSlider/PosterSlider.component";
+import axios from "axios";
 
 const HomePage = () => {
+
+  const [popularMovies, setPopularMovies]=useState([]);
+  const [topRatedMovies, setTopRatedMovies]=useState([]);
+  const [upcomingMovies, setUpcomingMovies]=useState([]);
+
+  useEffect(()=>{
+    const requestPopularMovies=async()=>{
+      const getPopularMovies=await axios.get("/movie/popular");
+      setPopularMovies(getPopularMovies.data.results);
+    };
+    requestPopularMovies();
+  },[]);
+
+  useEffect(()=>{
+    const requestTopRatedMovies=async()=>{
+      const getTopRatedMovies=await axios.get("/movie/top_rated");
+      setTopRatedMovies(getTopRatedMovies.data.results);
+    };
+    requestTopRatedMovies();
+  },[]);
+
+  useEffect(()=>{
+    const requestUpcomingMovies=async()=>{
+      const getUpcomingMovies=await axios.get("/movie/upcoming");
+      setUpcomingMovies(getUpcomingMovies.data.results);
+    };
+    requestUpcomingMovies();
+  },[]);
+
   return (
     <>
     <div className="flex flex-col gap-10">
@@ -27,17 +57,17 @@ const HomePage = () => {
           <div className="hidden md:flex">
             <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" alt="Premiere" className="w-full h-full" />
           </div>
-          <PosterSlider images={TempPosters} title='Premieres' subtitle="Brand new releases every Tuesday!" isDark={true} />
+          <PosterSlider images={popularMovies} title='Premieres' subtitle="Brand new releases every Tuesday!" isDark={true} />
         </div>
       </div>
       </div>
 
       <div className="container mx-auto px-4 my-8">
-        <PosterSlider  images={TempPosters} title="Online Streaming Events" isDark={false}/>
+        <PosterSlider  images={topRatedMovies} title="Online Streaming Events" isDark={false}/>
       </div>
 
       <div className="container mx-auto px-4 my-8">
-        <PosterSlider  images={TempPosters} title="Outdoor Events" isDark={false}/>
+        <PosterSlider  images={upcomingMovies} title="Outdoor Events" isDark={false}/>
       </div>
       
     </>
