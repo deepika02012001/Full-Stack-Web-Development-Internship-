a39f0346bd7d21793d4cf5b8686a98c6ee35d4d3
deepika02012001 <deepikak01022001@gmail.com>
2021-08-23 12:34:53 +0530

    add image to aws-s3

---
+++ Zomato-master/server/API/Image/index.js
@@ -0,0 +1,47 @@
+//libraries
+import express from "express";
+import passport from "passport";
+import multer from "multer";
+
+//database model
+import {ImageModel} from "../../database/allModels";
+
+//utilities
+import { s3Upload } from "../../Utils/AWS/s3";
+
+const Router = express.Router();
+
+//multer config
+const storage = multer.memoryStorage();
+const upload = multer({storage});
+
+/*
+Route      : /
+Description: uploads given image to aws s3 bucket, and saves file link to mongodb
+Params     : none
+Access     : Public
+method     : POST
+*/
+Router.post("/", upload.single("file"),async (req,res)=>{
+    try{
+        const file = req.file;
+
+        //s3 bucket options
+        const bucketOptions = {
+            Bucket: "zomato-images",
+            key: file.originalname,
+            Body: file.buffer,
+            ContentType: file.mimetype,
+            ACL: "public-read"
+        };
+
+        const uploadImage = await s3Upload(bucketOptions);
+
+        return res.status(200).json({uploadImage});
+
+    } catch(error){
+        return res.status(500).json({error: error.message});
+    }
+});
+
+export default Router;
