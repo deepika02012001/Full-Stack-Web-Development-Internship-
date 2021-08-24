e0f6109233af3c197fa5dbccb9eab7b261416352
deepika02012001 <deepikak01022001@gmail.com>
2021-08-24 21:14:10 +0530

    add validation for food api

--- Zomato-master/server/API/Food/index.js
+++ Zomato-master/server/API/Food/index.js
@@ -5,6 +5,9 @@ import passport from "passport";
 //database model
 import {FoodModel} from "../../database/allModels";
 
+//validation
+import { ValidateRestaurantId,ValidateCategory } from "../../validation/food";
+
 const Router = express.Router();
 
 /*
@@ -16,6 +19,8 @@ method     : GET
 */
 Router.get("/r/:_id", async (req,res)=>{
     try{
+        await ValidateRestaurantId(req.params);
+
         const {_id}=req.params;
         const foods = await FoodModel.find({restaurant: _id});
         return res.json({foods});
@@ -33,6 +38,8 @@ method     : GET
 */
 Router.get("/r/:category", async(req,res)=>{
     try{
+        await ValidateCategory(req.params);
+        
         const {category} = req.params;
         const foods = await FoodModel.find({ category: {$regex: category, $options: "i"}});
         return res.json({foods});
