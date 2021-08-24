201ecc77a09b9acfc8f7ff5c89e46afa73ec2004
deepika02012001 <deepikak01022001@gmail.com>
2021-08-24 11:42:59 +0530

    add order and review apis

--- Zomato-master/server/index.js
+++ Zomato-master/server/index.js
@@ -18,6 +18,8 @@ import Restaurant from "./API/Restaurant";
 import Food from "./API/Food";
 import Menu from "./API/Menu";
 import Image from "./API/Image";
+import Order from "./API/Order";
+import Review from "./API/Review";
 
 // Database connection
 import ConnectDB from "./database/connection";
@@ -41,6 +43,8 @@ zomato.use("/restaurant",Restaurant);
 zomato.use("/food",Food);
 zomato.use("/menu",Menu);
 zomato.use("/image",Image);
+zomato.use("/order",Order);
+zomato.use("/review",Review);
 
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
