cc6fc20a059446e8ddf21a4cc1a6c2f86dca5877
deepika02012001 <deepikak01022001@gmail.com>
2021-10-06 19:50:57 +0530

    add restaurant info and info buttons

--- Zomato-master/client/src/Layout/Restaurant.layout.jsx
+++ Zomato-master/client/src/Layout/Restaurant.layout.jsx
@@ -1,10 +1,15 @@
 import React,{ useState, useEffect }  from "react";
+import { TiStarOutline } from "react-icons/ti";
+import { RiDirectionLine, RiShareForwardLine } from "react-icons/ri";
+import { BiBookmarkPlus } from "react-icons/bi";
 
 //components
 import RestaurantNavbar from "../Components/Navbar/restaurantNavbar";
 import ImageGrid from "../Components/restaurant/ImageGrid";
+import RestaurantInfo from "../Components/restaurant/RestaurantInfo";
+import InfoButtons from "../Components/restaurant/InfoButtons";
 
-const RestaurantLayout =()=>{
+const RestaurantLayout =(props)=>{
     const [restaurant, setRestaurant] = useState({
         images: [
             "https://b.zmtcdn.com/data/pictures/chains/8/54308/da66b5379ee65458faaad20d37fef916.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
@@ -14,9 +19,12 @@ const RestaurantLayout =()=>{
             "https://b.zmtcdn.com/data/pictures/chains/8/54308/9fdbca8fc99781e6758620ffe94f4df6.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
             "https://b.zmtcdn.com/data/pictures/chains/8/54308/9aa51854ff41c62c3baa79a3f01791c5.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A"
         ],
-        name: "",
-        cuising: "",
-        address: "",
+        name: "Deepika",
+        restaurantRating: "4.3",
+        deliveryRating: "4.5",
+        rating: "4.5",
+        cuising: "North Indian cuisine",
+        address: "Bangalore",
       });
     return (
         <>
@@ -24,6 +32,31 @@ const RestaurantLayout =()=>{
       <RestaurantNavbar />
       <div className="container mx-auto px-4 lg:px-24 pb-10 ">
             <ImageGrid images={restaurant.images} />
+            <RestaurantInfo
+          name={restaurant?.name}
+          restaurantRating={restaurant?.rating || 0}
+          deliveryRating={restaurant?.rating || 0}
+          cuisine={restaurant?.cuising}
+          address={restaurant?.address}
+        />
+        <div className="my-4 flex flex-wrap gap-3">
+         <InfoButtons isActive>
+            <TiStarOutline /> Add Review
+          </InfoButtons>
+          <InfoButtons>
+            <RiDirectionLine /> Direction
+          </InfoButtons>
+          <InfoButtons>
+            <BiBookmarkPlus /> Bookmark
+          </InfoButtons>
+          <InfoButtons>
+            <RiShareForwardLine /> Share
+          </InfoButtons>
+        </div>
+        <div className="my-10">
+          
+        </div>
+        <div className="relative">{props.children}</div>
       </div>
         </>
     );
