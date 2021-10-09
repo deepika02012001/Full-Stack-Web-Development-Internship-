a6ea421f227e418be2c6f14f086095077368f4f8
deepika02012001 <deepikak01022001@gmail.com>
2021-10-09 18:10:35 +0530

    route

--- Zomato-master/client/src/App.jsx
+++ Zomato-master/client/src/App.jsx
@@ -24,27 +24,11 @@ function App() {
         <Redirect to="/restaurant/:id/overview" />
       </Route>
       <HomeLayoutHOC path="/:type" exact component={Home} />
-      <RestaurantLayoutHOC
-        path="/restaurant/:id/overview"
-        exact
-        component={Overview}
-      />
-      <RestaurantLayoutHOC
-        path="/restaurant/:id/order-online"
-        exact
-        component={OrderOnline}
-      />
+      <RestaurantLayoutHOC path="/restaurant/:id/overview" exact component={Overview} />
+      <RestaurantLayoutHOC path="/restaurant/:id/order-online" exact component={OrderOnline} />
       <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
-      <RestaurantLayoutHOC
-        path="/restaurant/:id/reviews"
-        exact
-        component={Temp}
-      />
-      <RestaurantLayoutHOC
-        path="/restaurant/:id/photos"
-        exact
-        component={Temp}
-      />
+      <RestaurantLayoutHOC path="/restaurant/:id/reviews" exact component={Temp} />
+      <RestaurantLayoutHOC path="/restaurant/:id/photos" exact component={Temp} />
     </>
   );
 }
