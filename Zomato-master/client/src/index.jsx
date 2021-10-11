1d929781ecca029d1caf91771539bf5c5c7d5b0d
deepika02012001 <deepikak01022001@gmail.com>
2021-10-11 21:17:00 +0530

    redux setup

--- Zomato-master/client/src/index.jsx
+++ Zomato-master/client/src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+
+import Store from "./Redux/store";
 
 //import css files
 import "slick-carousel/slick/slick.css";
@@ -10,9 +13,11 @@ import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={Store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
